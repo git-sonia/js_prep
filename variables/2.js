9f2688cf4e9ac78f6082b9ac3ed5b3ddfe6f8642
Sonia <tsonia5987@gmail.com>
2023-04-20 21:23:19 -0700

    input output

---
+++ variables/2.js
@@ -0,0 +1,5 @@
+let age = 20;
+console.log(`You are ${age} years old.`);
+for (let yearsPassed = 10; yearsPassed <= 40; yearsPassed += 10) {
+  console.log(`In ${yearsPassed} years, you will be ${age + yearsPassed} years old.`);
+}
