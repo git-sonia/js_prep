d210ca45415ae9638732020df369526fac55da5c
Sonia <tsonia5987@gmail.com>
2023-04-22 18:01:50 -0700

    function exercises

---
+++ functions/greeter.js
@@ -0,0 +1,8 @@
+let askFullName = () => {
+  let firstName = prompt("What is your first name? ");
+  let lastName = prompt("What is your last name? ");
+  return firstName + ' ' + lastName;
+}
+
+
+
---
+++ functions/multiply.js
@@ -0,0 +1,4 @@
+let multiply = (a, b) => a * b;
+let a = prompt("Enter the first number: ");
+let b = prompt("Enter the second number: ");
+console.log(`${a} * ${b} = ${multiply(a, b)}`);
