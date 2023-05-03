eb44c841608a10ef6fa475969d1706732f3cf61a
Sonia <tsonia5987@gmail.com>
2023-05-02 21:44:23 -0700

    functions 1 exercises

---
+++ JS100_exercises/functions_1/compare_length.js
@@ -0,0 +1,13 @@
+let compareByLength = (str1, str2) => {
+  if (str1.length < str2.length) {
+    return -1;
+  } else if (str1.length > str2.length) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+compareByLength('patience', 'perseverance'); // -1
+compareByLength('strength', 'dignity');      //  1
+compareByLength('humor', 'grace');           //  0
---
+++ JS100_exercises/functions_1/sum.js
@@ -0,0 +1,3 @@
+let sum = (a, b) => a + b;
+
+console.log(sum(22, 10));
