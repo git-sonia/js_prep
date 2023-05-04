96ffedf4cb0a37a996cee9f28fbbb305bd8a298e
Sonia <tsonia5987@gmail.com>
2023-05-04 11:06:11 -0700

    finsihed exercises

---
+++ JS100_exercises/objects/shallow_copy.js
@@ -0,0 +1,26 @@
+function clone(obj) {
+  let copy = {};
+  for (let key in obj) {
+    copy[key] = obj[key];
+  }
+  return copy;
+}
+
+let person = {
+  title: 'Duke',
+  name: {
+    value: 'Nukem',
+    isStageName: true
+  },
+  age: 33
+};
+
+let clonedPerson = clone(person);
+person.age = 34;
+
+console.log(person.age);       // 34
+console.log(clonedPerson.age); // 33
+
+person.name.isStageName = false;
+console.log(person.name.isStageName);       // false
+console.log(clonedPerson.name.isStageName); // false
